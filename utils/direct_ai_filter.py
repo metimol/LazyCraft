4e681dc4d2af225a38d91ce62156d53749edbe88
Vladyslav <154080171+metimol@users.noreply.github.com>
2026-04-05 19:32:57 +0200

    Fix sys message format

--- utils/direct_ai_filter.py
+++ utils/direct_ai_filter.py
@@ -3,11 +3,11 @@ from const import phrases
 
 async def filter_items_with_llm(items: list, user_prompt: str, radius: int) -> str:
     if not items:
-        return "Сегодня пусто"
+        return phrases.get_value("nothing_found")
 
     items_text = "\n".join([f"{i['title']} | Расстояние: {i['distance']} | Ссылка: {i['link']}" for i in items])
 
-    sys_msg = phrases.get_value("check_free_items".format(radius=radius, prompt=user_prompt, items=items_text))
+    sys_msg = phrases.get_value("check_free_items").format(radius=radius, prompt=user_prompt, items=items_text)
     resp = await ask_agent(sys_msg)
 
     return resp
