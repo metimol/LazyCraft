8cb5c10c83d76a2a89a8699ca68b06e283391681
Vladyslav <154080171+metimol@users.noreply.github.com>
2026-06-18 19:14:04 +0200

    Little better

--- utils/direct_ai_filter.py
+++ utils/direct_ai_filter.py
@@ -8,7 +8,16 @@ async def filter_items_with_llm(items: list, user_prompt: str, radius: int) -> s
 
     items_text = ""
     for i in items:
-        line = f"{i['title']} | Расстояние: {i['distance']} | Ссылка: {i['link']}\n"
+        if isinstance(i, dict):
+            title = i.get("title", "")
+            distance = i.get("distance", "")
+            link = i.get("link", "") or i.get("url", "")
+        else:
+            title = getattr(i, "title", "")
+            distance = getattr(i, "city", getattr(i, "distance", ""))
+            link = getattr(i, "url", getattr(i, "link", ""))
+
+        line = f"{title} | Расстояние: {distance} | Ссылка: {link}\n"
         if len(items_text) + len(line) > 600000:
             break
         items_text += line
